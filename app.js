387e811d60c02897a9efbb0afc6996e20e117276
Chad Schulz <chad.awesome@gmail.com>
2016-03-26 22:12:43 -0700

    Add testimonials data

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express'),
     path = require('path'),
-    app = express();
+    app = express(),
+
+    testimonials = require('./data/testimonials');
 
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
@@ -11,17 +13,17 @@ app.set('view engine', 'ejs');
 // app.use(express.compress());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res) {
-    res.status(400);
-    res.render('404');
-});
+function randomTestimonial() {
+	var index = Math.floor(Math.random() * testimonials.length);
+	return testimonials[index];
+}
 
 app.get('/', function(req, res) {
-    res.render('index');
+    res.render('index', { 'testimonial': randomTestimonial() });
 });
 
-app.get('/:id/', function(req, res) {
-    res.render(req.params.id);
+app.get('/about', function(req, res) {
+    res.render('about', { 'testimonial': randomTestimonial() });
 });
 
 app.listen(app.get('port'), function () {
