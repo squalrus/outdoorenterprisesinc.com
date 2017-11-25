1d3d8ed1634b4d1a12ba660b10e3ece19668db5e
Chad Schulz <chad.awesome@gmail.com>
2017-11-24 23:34:14 -0800

    Rename images (closes #4)

--- data/special-projects.js
+++ data/special-projects.js
@@ -1,9 +1,9 @@
 module.exports = {
     'waterfeatures': [
-        '9e3ac00c-584e-e061-79c8-45132664c3d7'
+        'waterfeatures01'
     ],
     'hardscapes':[
-        'c945a38f-d8fd-c6bd-b539-0974186fb51f',
-        'b6a9d73c-ffd8-c8eb-5b55-f9c585cce077'
+        'hardscapes01',
+        'hardscapes02'
     ]
 };
